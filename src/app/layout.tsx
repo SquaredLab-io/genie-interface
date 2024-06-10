03be41c927caef3e8be4ee3e42c1c84a488dbdd0
Sarthak Verma <sarthakvdev@gmail.com>
2024-06-11 02:05:27 +0530

    linting & prettier configured

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,18 +5,17 @@ import Header from "@components/common/Header";
 import { Metadata } from "next";
 import { meta } from "@utils/constants";
 
-const { APP_NAME, DESCRIPTION, KEYWORDS, URL, IMAGE, SITE_NAME, USERNAME } =
-  meta;
+const { APP_NAME, DESCRIPTION, KEYWORDS, URL, IMAGE, SITE_NAME, USERNAME } = meta;
 
 export const metadata: Metadata = {
   title: {
     template: `%s | ${APP_NAME}`,
-    default: APP_NAME,
+    default: APP_NAME
   },
   description: "Derivatives without liquidation.",
   icons: {
     icon: "/images/logo.svg",
-    apple: "apple-touch-icon-precomposed",
+    apple: "apple-touch-icon-precomposed"
   },
   keywords: KEYWORDS,
   applicationName: APP_NAME,
@@ -31,17 +30,17 @@ export const metadata: Metadata = {
       {
         url: "https://frontend-web-resources.s3.amazonaws.com/og-small.png",
         width: 800,
-        height: 600,
+        height: 600
       },
       {
         url: "https://frontend-web-resources.s3.amazonaws.com/og-large.png",
         width: 1800,
         height: 1600,
-        alt: "SquaredLabs",
-      },
+        alt: "SquaredLabs"
+      }
     ],
     locale: "en_US",
-    type: "website",
+    type: "website"
   },
   robots: {
     index: true,
@@ -50,20 +49,20 @@ export const metadata: Metadata = {
     googleBot: {
       index: true,
       follow: true,
-      nocache: true,
-    },
+      nocache: true
+    }
   },
   twitter: {
     title: "SquaredLabs",
     description: "Derivatives without liquidation.",
     images: [IMAGE],
-    card: 'summary_large_image',
-    creator: USERNAME,
-  },
+    card: "summary_large_image",
+    creator: USERNAME
+  }
 };
 
 export default function RootLayout({
-  children,
+  children
 }: Readonly<{
   children: React.ReactNode;
 }>) {
