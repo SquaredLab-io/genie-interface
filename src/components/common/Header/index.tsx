692856d81a2b662493f1d4a7261052bc68d1bb9b
Sarthak Verma <sarthakvdev@gmail.com>
2024-06-22 23:53:34 +0530

    feedback-modal in header

--- src/components/common/Header/index.tsx
+++ src/components/common/Header/index.tsx
@@ -1,19 +1,22 @@
 "use client";
 
-import { memo } from "react";
+import { memo, useState } from "react";
 import Image from "next/image";
 import { navigation } from "@lib/constants";
 import { usePathname } from "next/navigation";
 import clsx from "clsx";
 import Link from "next/link";
+import ConnectWallet from "../ConnectWallet";
+import FeedbackModal from "../feedback-modal";
 
 const Header = () => {
   const pathname = usePathname();
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
 
   return (
     <header>
       <nav
-        className="mx-auto flex bg-primary-gray items-center gap-x-12 py-3 px-4"
+        className="mx-auto flex bg-primary-gray justify-start items-center gap-x-12 py-3 px-4"
         aria-label="Global"
       >
         <div className="flex lg:flex-1 max-w-fit">
@@ -30,7 +33,7 @@ const Header = () => {
         </div>
         <div className="hidden sm:flex lg:gap-x-4 font-normal text-[13px]/[17.76px]">
           {navigation.map((item) => (
-            <a
+            <Link
               key={item.name}
               href={item.href}
               target={item.target}
@@ -38,12 +41,23 @@ const Header = () => {
                 "px-[10px]",
                 pathname !== item.href && "opacity-35 hover:opacity-70 transition-opacity"
               )}
+              aria-label={item.name}
             >
               {item.name}
-            </a>
+            </Link>
           ))}
+          <button
+            className={clsx("px-[10px] opacity-35 hover:opacity-70 transition-opacity")}
+            onClick={() => {
+              setIsModalOpen(true);
+            }}
+          >
+            Feedback & Support
+          </button>
         </div>
+        <ConnectWallet />
       </nav>
+      <FeedbackModal open={isModalOpen} setOpen={setIsModalOpen} />
     </header>
   );
 };
---
+++ src/components/common/feedback-modal.tsx
@@ -0,0 +1,88 @@
+import { useState, memo } from "react";
+import Image from "next/image";
+import ModalWrapper from "@components/common/Modal";
+import { Textarea } from "@components/ui/textarea";
+import { cn } from "@lib/utils";
+
+enum Feedback {
+  que = "Question",
+  req = "Request",
+  bug = "Bug"
+}
+
+const options = [
+  {
+    title: Feedback.que,
+    icon: "/icons/QuestionCircleIcon.svg"
+  },
+  {
+    title: Feedback.req,
+    icon: "/icons/BulbCircleIcon.svg"
+  },
+  {
+    title: Feedback.bug,
+    icon: "/icons/BugCircleIcon.svg"
+  }
+];
+
+const FeedbackModal = ({
+  open,
+  setOpen,
+  trigger
+}: {
+  open: boolean;
+  setOpen: (value: boolean) => void;
+  trigger?: React.ReactNode;
+}) => {
+  const [selectedFeedback, setSelectedFeedback] = useState<Feedback | null>(null);
+  return (
+    <ModalWrapper
+      open={open}
+      onOpenChange={setOpen}
+      title="Contact SquaredLabs Team"
+      trigger={trigger}
+      className="max-w-5xl"
+      footer={
+        <div className="inline-flex items-center justify-between w-full">
+          <p>
+            You can also email us at{" "}
+            <a
+              className="text-gradient-blue"
+              href="mailto:support@squaredlabs.io"
+              target="_blank"
+            >
+              support@squaredlabs.io
+            </a>
+          </p>
+          <div className="font-medium text-xl/6">
+            <button className="py-[10px] px-6 bg-transparent text-white border border-white rounded-2xl mr-6">
+              Help Center
+            </button>
+            <button className="py-[10px] px-6 bg-white text-black rounded-2xl">
+              Send
+            </button>
+          </div>
+        </div>
+      }
+    >
+      <div className="flex flex-row gap-3">
+        {options.map((option) => (
+          <button
+            key={option.title}
+            className={cn(
+              "inline-flex rounded-full border border-[#ECECEC] hover:bg-white/10 py-[6px] px-3 gap-3 transition-colors",
+              selectedFeedback === option.title && "bg-white/10"
+            )}
+            onClick={() => setSelectedFeedback(option.title)}
+          >
+            <Image src={option.icon} width={26} height={26} alt={option.title} />
+            {Feedback.que}
+          </button>
+        ))}
+      </div>
+      <Textarea placeholder="Enter text here" className="min-w-[954px] h-[228px]" />
+    </ModalWrapper>
+  );
+};
+
+export default FeedbackModal;
