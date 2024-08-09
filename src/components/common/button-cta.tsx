637cafbd89a31131f3e7964092f44b16bf0328a2
Sarthak Verma <sarthakvdev@gmail.com>
2024-08-09 23:40:33 +0530

    revamp: disabled cta button

--- src/components/common/button-cta.tsx
+++ src/components/common/button-cta.tsx
@@ -16,7 +16,7 @@ const ButtonCTA = forwardRef<HTMLButtonElement, PropsType>(
       <button
         ref={ref}
         className={clsx(
-          "inline-flex items-center justify-center font-sans-ibm-plex font-medium text-[14px]/6 text-white text-center py-3 disabled:opacity-70 disabled:cursor-not-allowed uppercase transition-all duration-200 bg-gradient-to-l from-secondary-blue via-secondary-blue to-primary-cyan bg-size-200 bg-pos-100 hover:bg-pos-0",
+          "inline-flex items-center justify-center font-sans-ibm-plex font-medium text-[14px]/6 text-white text-center py-3 disabled:opacity-70 disabled:cursor-not-allowed uppercase transition-all duration-200 bg-gradient-to-l from-secondary-blue via-secondary-blue to-primary-cyan bg-size-200 bg-pos-100 hover:bg-pos-0 disabled:from-[#373C40] disabled:via-[#373C40] disabled:to-[#373C40]",
           className
         )}
         disabled={disabled}
