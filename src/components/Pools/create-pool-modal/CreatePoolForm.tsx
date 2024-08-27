4eb8daa3a06a9fae82b96d12383276bfa8757920
Sarthak Verma <sarthakvdev@gmail.com>
2024-08-27 20:58:19 +0530

    fix: create-pool-form ui fixes

--- src/components/Pools/create-pool-modal/CreatePoolForm.tsx
+++ src/components/Pools/create-pool-modal/CreatePoolForm.tsx
@@ -1,12 +1,12 @@
-import { ReactNode, useCallback, useEffect, useMemo, useRef, useState } from "react";
-import { useForm, Controller } from 'react-hook-form';
-import { zodResolver } from "@hookform/resolvers/zod"
+import { ReactNode } from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 import {
   Form,
   FormControl,
   FormField,
   FormItem,
-  FormMessage,
+  FormMessage
 } from "@/components/ui/form";
 import SliderBar from "@components/common/slider-bar";
 import { Input } from "@components/ui/input";
@@ -23,27 +23,16 @@ interface BoxInfo {
 
 const BoxInfo = ({ title, description, children }: BoxInfo) => (
   <div className="flex flex-row items-start justify-between">
-    <div className="flex flex-col gap-y-2 w-1/2 float-left">
+    <div className="flex flex-col gap-y-2 w-1/2 max-w-[282px] float-left">
       <h4 className="font-medium text-lg/[18px]">{title}</h4>
-      {description && <p className="text-base/6 text-[#94A3B8]">{description}</p>}
+      {description && <p className="text-sm/5 text-[#94A3B8]">{description}</p>}
     </div>
     <div className="w-1/2 float-right">{children}</div>
   </div>
 );
 
 export default function CreatePoolForm() {
-
   const VAULT_ADDRESS = "0x428084313F9dCc38e9d0cB51dBBe466c8300a35c";
-
-  // const initialLiquidityTimeoutRef = useRef<number | null>(null);
-
-  /* useEffect(() => {
-    return () => {
-      if (initialLiquidityTimeoutRef.current) {
-        clearTimeout(initialLiquidityTimeoutRef.current);
-      }
-    }
-  }, []) */
 
   const form = useForm<CreatePoolFormSchema>({
     resolver: zodResolver(formSchema),
@@ -52,28 +41,13 @@ export default function CreatePoolForm() {
       power: 2,
       sqlFee: 100,
       halfLife: 0,
-      priceUpdateFactor: 0,
+      priceUpdateFactor: 0
     },
-    mode: 'onChange'
+    mode: "onChange"
   });
 
-  /*  const [isSqlDiscValid, sqlDiscMin, sqlDiscMax] = useMemo(() => {
-     const disc = sqlDiscount[0];
-     if (disc > 0 && disc <= 2000) {
-       return [true, false, false];
-     }
-     return [false, disc < 0, disc > 2000];
-   }, [sqlDiscount]); */
-
-  /* const isValidUpdate =
-    halfLife > 0 &&
-    !isNaN(halfLife) &&
-    priceUpdateFactor > 0 &&
-    !isNaN(priceUpdateFactor) &&
-    !isSqlDiscValid; */
-
   function onSubmit(values: CreatePoolFormSchema) {
-    console.log(values);
+    console.log("create pool form", values);
   }
 
   return (
@@ -106,7 +80,7 @@ export default function CreatePoolForm() {
               control={form.control}
               name="initialLiquidity"
               render={({ field, formState: { errors } }) => (
-                <FormItem >
+                <FormItem>
                   <FormControl>
                     <>
                       <Input
@@ -116,7 +90,10 @@ export default function CreatePoolForm() {
                           return field.onChange(parseFloat(e.target.value));
                         }}
                         type="number"
-                        className={cn("p-4 bg-transparent mb-4", errors.initialLiquidity && "border-[#FF3318]")}
+                        className={cn(
+                          "p-4 bg-transparent mb-4",
+                          errors.initialLiquidity && "border-[#FF3318]"
+                        )}
                       />
                       <FormMessage className="text-[#FF3318] text-sm/5 -mt-2 mb-3" />
                       <SliderBar
@@ -143,7 +120,7 @@ export default function CreatePoolForm() {
               control={form.control}
               name="power"
               render={({ field, formState: { errors } }) => (
-                <FormItem >
+                <FormItem>
                   <FormControl>
                     <SliderBar
                       {...field}
@@ -151,7 +128,7 @@ export default function CreatePoolForm() {
                       setValue={(value) => field.onChange(value)}
                       min={2}
                       max={32}
-                      // step={0.001} 
+                      // step={0.001}
                       step={1}
                       className="mt-5"
                       indices={[2, 7, 12, 17, 22, 27, 32]}
@@ -162,54 +139,16 @@ export default function CreatePoolForm() {
               )}
             />
           </BoxInfo>
-          <BoxInfo
-            title="SQL Fees"
-            description="Which token pair would you like to add liquidity to."
-          >
-            <FormField
-              control={form.control}
-              name="sqlFee"
-              render={({ field, formState: { errors } }) => (
-                <FormItem >
-                  <FormControl>
-                    <>
-                      <Input
-                        placeholder="0"
-                        value={field.value}
-                        onChange={(e) => {
-                          return field.onChange(parseFloat(e.target.value));
-                        }}
-                        type="number"
-                        className={cn("p-4 bg-transparent mb-4", errors.sqlFee && "border-[#FF3318]")}
-                      />
-                      <FormMessage className="text-[#FF3318] text-sm/5 -mt-2 mb-3" />
-                      <SliderBar
-                        {...field}
-                        value={field.value}
-                        setValue={(value) => field.onChange(value)}
-                        min={100}
-                        max={2000}
-                        step={1}
-                        className="mt-5"
-                        indices={[100, 575, 1050, 1525, 2000]}
-                      />
-                    </>
-                  </FormControl>
-                </FormItem>
-              )}
-            />
-          </BoxInfo>
           <p className="w-full text-right font-normal text-base/6 text-[#94A3B8]">
-            Position Discount : <span className="font-bold text-white">15.5%</span>
+            Position Discount: <span className="font-bold text-white">15.5%</span>
           </p>
         </div>
         <Separator />
         {/* Advanced Section */}
         <div className="p-6">
-          <h4 className="font-normal text-base/6 text-[#94A3B8] mb-6">Advanced</h4>
-          <div className="flex flex-row items-start justify-between mb-20">
-            <h5>Position Half Life</h5>
-            <div className="w-1/2">
+          <h4 className="font-normal text-base/6 text-[#94A3B8] mb-7">Advanced</h4>
+          <div className="w-full flex flex-col gap-20">
+            <BoxInfo title="Position Half Life">
               <FormField
                 control={form.control}
                 name="halfLife"
@@ -224,7 +163,10 @@ export default function CreatePoolForm() {
                             return field.onChange(parseFloat(e.target.value));
                           }}
                           type="number"
-                          className={cn("p-4 bg-transparent mb-4", errors.halfLife && "border-[#FF3318]")}
+                          className={cn(
+                            "p-4 bg-transparent mb-4",
+                            errors.halfLife && "border-[#FF3318]"
+                          )}
                         />
                         <FormMessage className="text-[#FF3318] text-sm/5 -mt-2 mb-3" />
                         <SliderBar
@@ -242,11 +184,8 @@ export default function CreatePoolForm() {
                   </FormItem>
                 )}
               />
-            </div>
-          </div>
-          <div className="flex flex-row items-start justify-between mb-20">
-            <h5>Price Update Factor</h5>
-            <div className="w-1/2">
+            </BoxInfo>
+            <BoxInfo title="Price Update Factor">
               <FormField
                 control={form.control}
                 name="priceUpdateFactor"
@@ -261,7 +200,10 @@ export default function CreatePoolForm() {
                             return field.onChange(parseFloat(e.target.value));
                           }}
                           type="number"
-                          className={cn("p-4 bg-transparent mb-4", errors.priceUpdateFactor && "border-[#FF3318]")}
+                          className={cn(
+                            "p-4 bg-transparent mb-4",
+                            errors.priceUpdateFactor && "border-[#FF3318]"
+                          )}
                         />
                         <FormMessage className="text-[#FF3318] text-sm/5 -mt-2 mb-3" />
                         <SliderBar
@@ -279,13 +221,18 @@ export default function CreatePoolForm() {
                   </FormItem>
                 )}
               />
+            </BoxInfo>
+            <div className="w-full">
+              <ButtonCTA
+                className="w-1/2 rounded-lg float-right"
+                disabled={!(form.formState.isValid && form.formState.isDirty)}
+              >
+                Create Pool
+              </ButtonCTA>
             </div>
           </div>
-          <ButtonCTA className="w-1/2 rounded-lg float-right" disabled={!(form.formState.isValid && form.formState.isDirty)}>
-            Create Pool
-          </ButtonCTA>
         </div>
       </form>
     </Form>
-  )
+  );
 }
