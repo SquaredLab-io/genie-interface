108257f16614de2748dcaab04cd7b1a02f502569
Greyn1 <54180785+Greyn1@users.noreply.github.com>
2024-08-28 22:23:59 +0530

    update: displaying data from coingecko on the application

--- src/components/Trade/MarketData.tsx
+++ src/components/Trade/MarketData.tsx
@@ -32,12 +32,22 @@ const MarketData = () => {
   const underlying = selectedPool()?.underlying;
   const {
     marketData,
-    isMarketDataFetching,
+    isMarketDataLoading,
     refetchMarketData
   } = useCurrencyPrice(underlying);
-  
   return (
+    <>
     <div className="px-4 pb-4 w-full">
+      <h3 className="font-medium text-sm/[54px]">Underlying Market Data</h3>
+      <div className="flex flex-col gap-2 font-normal text-xs/[14px]">
+        <Marker label={"Market Cap"} value={marketData ? (marketData?.market_cap ?? 0).toString() : "-"} fetching={isMarketDataLoading} />
+        <Marker label={"Volume (24h)"} value={"-"} fetching={false} />
+        <Marker label={"Day Change"} value={marketData ? (`${marketData.price_change_percentage_24h }%`?? 0).toString() : "-"} fetching={isMarketDataLoading} showChange={true} />
+        <Marker label={"Max Total Supply"} value={marketData ? (marketData.max_supply ?? "-").toString() : "-"} fetching={isMarketDataLoading} />
+      </div>
+    </div>
+
+    {/* <div className="px-4 pb-4 w-full">
       <h3 className="font-medium text-sm/[54px]">Underlying Market Data</h3>
       <div className="flex flex-col gap-2 font-normal text-xs/[14px]">
         <Marker label={"Market Cap"} value={"-"} fetching={false} />
@@ -45,7 +55,8 @@ const MarketData = () => {
         <Marker label={"Day Change"} value={"-"} fetching={false} showChange={true} />
         <Marker label={"Max Total Supply"} value={"-"} fetching={false} />
       </div>
-    </div>
+    </div> */}
+    </>
   );
 };
 
