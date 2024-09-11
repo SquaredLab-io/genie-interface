0e3c542afd5024709ff2b8cb9ce7137480d117f1
Sarthak Verma <sarthakvdev@gmail.com>
2024-09-11 16:28:36 +0530

    fix: trade-interface loading jitter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 "use client";
 
 import dynamic from "next/dynamic";
+import { useRouter } from "next/navigation";
 import { usePools } from "@lib/hooks/usePools";
 import Loading from "./loading";
+import { Button } from "@components/ui/button";
 
 // Trade Interface imported dynamically
 const Trade = dynamic(() => import("@components/Trade").then((mod) => mod.default));
@@ -11,10 +13,20 @@ const Trade = dynamic(() => import("@components/Trade").then((mod) => mod.defaul
  * Trade Interface - Currently set as the Homepage of Genie
  */
 export default function Home() {
-  const { pools, isFetching } = usePools();
+  const { pools, isFetching, status } = usePools();
+  const router = useRouter();
 
-  if (isFetching && !pools) return <Loading />;
-  else if (pools && pools.length === 0) {
+  if (isFetching && status === "pending") return <Loading />;
+  else if (status === "error") {
+    return (
+      <main className="page-center flex flex-col items-center justify-center">
+        <h2 className="text-4xl tracking-wide mb-10">Something went wrong!</h2>
+        <Button onClick={() => router.refresh()} variant={"secondary"}>
+          Try again
+        </Button>
+      </main>
+    );
+  } else if (status === "success" && pools?.length === 0) {
     return (
       <main className="page-center items-center justify-center text-3xl">
         404: Pools not found
