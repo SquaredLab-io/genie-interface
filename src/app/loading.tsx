a515750974898b14fc2ac4b375092da81666c5bb
Sarthak Verma <sarthakvdev@gmail.com>
2024-09-30 22:17:47 +0530

    fix: dynamic imports fixed

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -3,7 +3,7 @@
 import { ReactNode } from "react";
 import LoadingLogo from "@components/icons/loading-logo";
 
-const Loading = (): ReactNode => (
+const Loading = () => (
   <main className="page-center size-full flex-col-center gap-5 font-sans-ibm-plex border-t border-secondary-gray">
     <LoadingLogo />
   </main>
