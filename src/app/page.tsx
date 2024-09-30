c5f5788b0fa2ed7494a827187b9ab2124e25e57c
Sarthak Verma <sarthakvdev@gmail.com>
2024-09-30 17:26:10 +0530

    feat: making app desktop only

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,43 +1,51 @@
 "use client";
 
-import dynamic from "next/dynamic";
-import { useRouter } from "next/navigation";
 import { usePools } from "@lib/hooks/usePools";
 import Loading from "./loading";
-// import { Button } from "@components/ui/button";
-
-// Trade Interface imported dynamically
-const Trade = dynamic(() => import("@components/Trade").then((mod) => mod.default));
+import { useWindowSize } from "usehooks-ts";
+import { useMemo } from "react";
+import MobileInfoScreen from "@components/common/MobileInfoScreen";
+import Trade from "@components/Trade";
 
 /**
  * Trade Interface - Currently set as the Homepage of Genie
  */
 export default function Home() {
+  const { width } = useWindowSize();
   const { pools, isFetching, status } = usePools();
-  const router = useRouter();
 
-  if (isFetching && status === "pending") return <Loading />;
-  // else if (status === "error") {
-  //   return (
-  //     <main className="page-center flex flex-col items-center justify-center">
-  //       <h2 className="text-4xl tracking-wide mb-10">Something went wrong!</h2>
-  //       <Button onClick={() => router.refresh()} variant={"secondary"}>
-  //         Try again
-  //       </Button>
-  //     </main>
-  //   );
-  // }
-  else if (status === "success" && pools?.length === 0) {
+  const TradeInterface = () => {
+    if ((isFetching && status === "pending") || width === 0) return <Loading />;
+    else if (status === "success" && pools?.length === 0) {
+      return (
+        <main className="page-center items-center justify-center text-3xl">
+          404: Pools not found
+        </main>
+      );
+    }
     return (
-      <main className="page-center items-center justify-center text-3xl">
-        404: Pools not found
+      <main className="page-center">
+        <Trade />
       </main>
     );
-  }
+  };
 
-  return (
-    <main className="page-center">
-      <Trade />
-    </main>
-  );
+  const render = useMemo(() => {
+    return width <= 1024 ? MobileInfoScreen : <TradeInterface />;
+  }, [width]);
+
+  return render;
 }
+
+/*
+   else if (status === "error") {
+     return (
+       <main className="page-center flex flex-col items-center justify-center">
+         <h2 className="text-4xl tracking-wide mb-10">Something went wrong!</h2>
+         <Button onClick={() => router.refresh()} variant={"secondary"}>
+           Try again
+         </Button>
+       </main>
+     );
+   }
+ */
