ad9b128958cdd1762caeb8b6908273a4f93d11ed
Sarthak Verma <sarthakvdev@gmail.com>
2024-10-08 01:28:47 +0530

    ui: point-popover, pointer assets, popover fixes

--- src/components/common/Header/index.tsx
+++ src/components/common/Header/index.tsx
@@ -9,11 +9,15 @@ import { meta, navigation } from "@lib/constants";
 import FeedbackModal from "./feedback-modal";
 import ConnectWallet from "../ConnectWallet";
 import FaucetModal from "@components/common/Header/faucet-modal";
+import PointsPopover from "./points-popover";
+import { useAccount } from "wagmi";
 
 const Header = () => {
   const pathname = usePathname();
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
   const [isFaucetOpen, setIsFaucetOpen] = useState<boolean>(false);
+
+  const { isConnected } = useAccount();
 
   return (
     <header className="flex flex-row flex-grow py-4 px-5 justify-between font-sans-ibm-plex max-w-full">
@@ -68,6 +72,22 @@ const Header = () => {
         </div>
       </nav>
       <div className="hidden lg:inline-flex gap-6">
+        {isConnected && (
+          <PointsPopover>
+            <button className="inline-flex items-center gap-x-1 text-sm max-w-fit px-2 -mx-2 select-none hover:scale-105 active:scale-95 transition-transform duration-200">
+              <Image
+                src="/icons/PointsIcon.svg"
+                alt="Genie Points | GPoints"
+                height={24}
+                width={24}
+              />
+              <span className="font-normal leading-5 ml-1">25 Gpoints</span>
+              <span className="font-medium leading-4 font-sans-ibm-plex text-primary-green bg-primary-green/10 py-0.5 px-1 rounded-base">
+                1.5x
+              </span>
+            </button>
+          </PointsPopover>
+        )}
         <ConnectWallet />
       </div>
       {isFaucetOpen && <FaucetModal open={isFaucetOpen} setOpen={setIsFaucetOpen} />}
