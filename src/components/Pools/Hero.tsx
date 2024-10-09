a1fa63b20b38fa9a83c524eeb22c0fbedf63bf55
Sarthak Verma <sarthakvdev@gmail.com>
2024-10-10 01:13:06 +0530

    chore: minor pools page styling fix

--- src/components/Pools/Hero.tsx
+++ src/components/Pools/Hero.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 const Hero = () => {
   return (
     <div className="flex flex-col gap-2">
-      <h1 className="heading-gradient font-medium text-[32px] leading-9">Pools</h1>
+      <h1 className="heading-gradient font-medium text-[32px]/9">Pools</h1>
       <h4 className="font-normal text-base/[22px] text-[#98B0C1]">
         Earn fees & rewards by deploying liquidity into Pools.
         <Link
