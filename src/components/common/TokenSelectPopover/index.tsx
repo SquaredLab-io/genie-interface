b93be62d6de31be7795b518d17c3b6457ecfbb31
Sarthak Verma <sarthakvdev@gmail.com>
2024-10-11 21:40:09 +0530

    feature: referral-modal // functionality

--- src/components/common/TokenSelectPopover/index.tsx
+++ src/components/common/TokenSelectPopover/index.tsx
@@ -3,7 +3,7 @@ import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@components/ui/tabs";
 import { cn } from "@lib/utils";
 import { Separator } from "@components/ui/separator";
-import { useFilteredPools } from "../../../lib/hooks/useFilteredPools";
+import { useFilteredPools } from "@lib/hooks/useFilteredPools";
 import SearchInput from "./SearchInput";
 import PoolsList from "./PoolsList";
 import { PopoverSizes } from "@lib/types/common";
