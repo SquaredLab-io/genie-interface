63d3509eed02dfa4be336a2639dac95891ce18c1
Sarthak Verma <sarthakvdev@gmail.com>
2024-10-15 18:31:19 +0530

    opti: tv_chart re-rendering

--- src/components/Trade/TradeChart/index.tsx
+++ src/components/Trade/TradeChart/index.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useRef, memo, MutableRefObject } from "react";
+import { useEffect, useRef, memo, MutableRefObject, useState, useCallback } from "react";
 import {
   ConfigurationData,
   ExternalFeed,
@@ -26,44 +26,49 @@ interface PropsType {
 const TradeChart = ({ potentia }: PropsType) => {
   const { selectedPool } = usePoolsStore();
   const { tradeType } = useTradeStore();
+  const [dataFeed, setDataFeed] = useState<ExternalFeed | null>(null);
 
   // Chart Container and Widget Refs
   const chartContainerRef =
     useRef<HTMLDivElement>() as MutableRefObject<HTMLInputElement>;
   const tvWidgetRef = useRef<IChartingLibraryWidget | null>(null);
 
+  const getSymbol = useCallback(() => {
+    if (selectedPool()) {
+      return `${selectedPool()?.underlying}^${selectedPool()?.power} ${tradeType.toUpperCase()}`;
+    }
+    return "";
+  }, [selectedPool, tradeType]);
+
   useEffect(() => {
-    async function initChart() {
-      if (!potentia || !selectedPool()) return;
+    async function initDatafeed() {
+      if (!potentia) return;
+      const newDataFeed = await getPotentiaDataFeed(potentia, ConfigurationData, true);
+      setDataFeed(newDataFeed);
+    }
+    initDatafeed();
+  }, [potentia]);
 
-      const Datafeed: ExternalFeed = await getPotentiaDataFeed(
-        potentia,
-        ConfigurationData,
-        true
-      );
-      const tokenSymbol = `${selectedPool()?.underlying}^${selectedPool()?.power} ${tradeType.toUpperCase()}`;
+  useEffect(() => {
+    if (!dataFeed || !selectedPool()) return;
 
+    function initChart() {
       // Retrieve the saved timezone from localStorage
       const savedTimezone =
         (localStorage.getItem("chartTimezone") as "exchange" | Timezone | undefined) ||
         widgetProps.timezone;
 
       const widgetOptions: ChartingLibraryWidgetOptions = {
-        symbol: tokenSymbol,
+        symbol: getSymbol(),
         // BEWARE: no trailing slash is expected in feed URL
-        datafeed: Datafeed,
+        datafeed: dataFeed!,
         timezone: savedTimezone,
         interval: widgetProps.interval as ResolutionString,
         container: chartContainerRef.current,
         library_path: widgetProps.library_path,
         locale: widgetProps.locale as LanguageCode,
-        disabled_features: [
-          "use_localstorage_for_settings",
-          "header_symbol_search",
-          "symbol_search_hot_key",
-          "symbol_info"
-        ],
-        enabled_features: ["study_templates", "timezone_menu"],
+        disabled_features: widgetProps.disabled_features,
+        enabled_features: widgetProps.enabled_features,
         charts_storage_url: widgetProps.charts_storage_url,
         charts_storage_api_version: widgetProps.charts_storage_api_version,
         client_id: widgetProps.client_id,
@@ -71,40 +76,45 @@ const TradeChart = ({ potentia }: PropsType) => {
         fullscreen: widgetProps.fullscreen,
         autosize: widgetProps.autosize,
         theme: widgetProps.theme,
-        debug: false,
+        debug: widgetProps.debug,
         symbol_search_request_delay: widgetProps.symbol_search_request_delay,
         auto_save_delay: widgetProps.auto_save_delay,
         toolbar_bg: widgetProps.toolbar_bg,
-        overrides: {
-          "paneProperties.background": "#0C1820",
-          "paneProperties.backgroundType": "solid"
-        }
+        overrides: widgetProps.overrides
       };
 
       const tvWidget = new widget(widgetOptions);
       tvWidgetRef.current = tvWidget;
 
       tvWidget.onChartReady(() => {
-        // console.log("Chart is ready");
         injectCustomCSS(tvWidget);
-
         tvWidget.subscribe("onAutoSaveNeeded", () => {
           const currentTimezone = tvWidget.chart().getTimezoneApi().getTimezone();
           localStorage.setItem("chartTimezone", currentTimezone.id);
         });
       });
-
-      // Cleanup function
-      return () => {
-        if (tvWidgetRef.current) {
-          tvWidgetRef.current.remove();
-          tvWidgetRef.current = null;
-        }
-      };
     }
 
-    initChart();
-  }, [widgetProps, selectedPool, tradeType, potentia]);
+    // Only re-initialise the chart when it's doesn't exists
+    if (!tvWidgetRef.current) {
+      initChart();
+    } else {
+      // Update existing chart
+      tvWidgetRef.current.setSymbol(
+        getSymbol(),
+        widgetProps.interval as ResolutionString,
+        () => {}
+      );
+    }
+
+    // Cleanup function
+    return () => {
+      if (tvWidgetRef.current) {
+        tvWidgetRef.current.remove();
+        tvWidgetRef.current = null;
+      }
+    };
+  }, [dataFeed, selectedPool, tradeType, getSymbol]);
 
   function injectCustomCSS(tvWidget: IChartingLibraryWidget) {
     tvWidget.addCustomCSSFile(
