f1c97357eacba9b8324013531ca333048d86a3be
Sarthak Verma <sarthakvdev@gmail.com>
2024-10-18 15:39:42 +0530

    ui: icon / copy for long/short trade

--- src/components/Points/sections/stats/columns.tsx
+++ src/components/Points/sections/stats/columns.tsx
@@ -8,23 +8,26 @@ export const rewardsColumns: ColumnDef<UserPointHistory>[] = [
     id: "action",
     accessorKey: "action",
     header: () => (
-      <div className="pl-6 pb-4 w-full flex items-start">
+      <div className="pl-6 w-full flex items-start">
         <span>Recent Rewards</span>
       </div>
     ),
     cell: ({ row }) => {
       const { points, action } = row.original;
+      const isLong = action.toLowerCase() === "close long";
       return (
         <div className="pl-6 whitespace-nowrap flex flex-row items-center gap-x-4">
           <Image
-            src="/icons/PnlPositiveIcon.svg"
+            src={isLong ? "/icons/LongPositionIcon.svg" : "/icons/ShortPositionIcon.svg"}
             alt="Rewards action"
             height={45}
             width={45}
           />
           <div className="flex flex-col gap-y-1 text-left text-sm/5 font-bold pl-6 py-3">
             <p className="text-white opacity-95">Earned {points} Gpoints</p>
-            <p className="font-normal text-[#9299AA]">{action} Position</p>
+            <p className="font-normal text-[#9299AA]">
+              {isLong ? "Closed Long" : "Closed Short"} Position
+            </p>
           </div>
         </div>
       );
@@ -34,7 +37,7 @@ export const rewardsColumns: ColumnDef<UserPointHistory>[] = [
     id: "points",
     accessorKey: "points",
     header: () => (
-      <div className="w-full pb-4">
+      <div className="w-full">
         <span>Points</span>
       </div>
     ),
@@ -47,7 +50,7 @@ export const rewardsColumns: ColumnDef<UserPointHistory>[] = [
     id: "date",
     accessorKey: "date",
     header: () => (
-      <div className="pb-4 pr-4 w-full text-center">
+      <div className="pr-4 w-full text-center">
         <span>Date</span>
       </div>
     ),
