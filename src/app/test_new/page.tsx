a99777459fe4d212223ba3fe8e742deb6c7ddb84
Sarthak Verma <sarthakvdev@gmail.com>
2024-11-28 23:30:59 +0530

    chore: tradeOption while opening trade-drawer

--- src/app/test_new/page.tsx
+++ src/app/test_new/page.tsx
@@ -1,33 +1,29 @@
 "use client";
 
+import { useMemo } from "react";
 import { useAccount } from "wagmi";
-import { useIsClient, useWindowSize } from "usehooks-ts";
+import { useIsClient } from "usehooks-ts";
 import _useTokenBalance from "@lib/hooks/useTokenBalance";
 import { _getDecimalAdjusted } from "@lib/utils/formatting";
 import Loading from "@app/loading";
-import TradeButton from "../../components/Trade/trade-drawer-section/trade-buttons";
+import TradeDrawerSection from "@components/Trade/trade-drawer-section";
 
 export default function TestNew() {
-  const { width } = useWindowSize();
   const isClient = useIsClient();
   const { isConnected, isConnecting, address } = useAccount();
 
-  if (width === 0 || !isClient) return <Loading />;
+  const connectionStatus = useMemo(() => {
+    if (isConnecting) return "connecting...";
+    else if (isConnected) return "connected";
+    return "not connected";
+  }, [isConnecting, isConnected]);
+
+  if (!isClient) return <Loading />;
   return (
     <main className="relative page-center items-center justify-center gap-3">
-      <span>
-        Wallet status:{" "}
-        {isConnecting ? "Connecting..." : isConnected ? "Connected" : "Not Connected"}
-      </span>
+      <span>user status: {connectionStatus}</span>
       {isConnected && <span>{address}</span>}
-      <div className="flex flex-row items-center gap-2 mb-4 w-full max-h-fit absolute bottom-7 px-3">
-        <TradeButton variant={"long"} className="w-1/2 float-left">
-          Long
-        </TradeButton>
-        <TradeButton variant={"short"} className="w-1/2 float-right">
-          Short
-        </TradeButton>
-      </div>
+      <TradeDrawerSection />
     </main>
   );
 }
--- src/components/Trade/trade-drawer-section/index.tsx
+++ src/components/Trade/trade-drawer-section/index.tsx
@@ -1,9 +1,18 @@
-import TradeButton from "./trade-buttons";
 import { useState } from "react";
+import { useAccount } from "wagmi";
+import TradeButton from "./trade-buttons";
 import TradeDrawerContainer from "./trade-drawer-container";
+import { useTradeStore } from "@store/tradeStore";
+import { TradeOptions } from "@lib/types/enums";
 
 const TradeDrawerSection = () => {
+  // Drawer state
   const [isTradeDrawer, setIsTradeDrawer] = useState(false);
+  const { setTradeType } = useTradeStore();
+
+  const { isConnected } = useAccount();
+
+  if (!isConnected) return <></>;
 
   return (
     <div className="flex lg:hidden flex-row items-center gap-2 mb-4 w-full max-h-fit absolute bottom-4 px-3">
@@ -11,6 +20,7 @@ const TradeDrawerSection = () => {
         variant={"long"}
         className="w-1/2 float-left"
         onClick={() => {
+          setTradeType(TradeOptions.long);
           setIsTradeDrawer(true);
         }}
       >
@@ -20,6 +30,7 @@ const TradeDrawerSection = () => {
         variant={"short"}
         className="w-1/2 float-right"
         onClick={() => {
+          setTradeType(TradeOptions.short);
           setIsTradeDrawer(true);
         }}
       >
