4884003cd56d0f91de84f9e5e93b01aabf9f2f06
Sarthak Verma <sarthakvdev@gmail.com>
2024-11-29 04:08:18 +0530

    feat ui: referral and stats page mobile

--- src/components/Points/sections/stats/stats-card.tsx
+++ src/components/Points/sections/stats/stats-card.tsx
@@ -16,9 +16,9 @@ const StatsCard: FC<StatsCardProps> = ({
   textStyle = "text-white"
 }) => {
   return (
-    <div className="col-span-1 min-w-[230px] w-fit rounded-lg flex flex-col gap-y-2 p-4 items-start bg-[#14252E] hover:bg-[#142F41] transition-colors duration-200 ease-in">
+    <div className="col-span-1 min-w-[179px] md:min-w-[230px] w-full rounded-lg flex flex-col gap-y-2 p-4 items-start bg-[#14252E] hover:bg-[#142F41] transition-colors duration-200 ease-in">
       <Image src={icon} height={40} width={40} alt={label} />
-      <h3 className="font-normal text-base/[14.5px] text-[#858D92]">{label}</h3>
+      <h3 className="font-normal text-xs/[14.5px] md:text-base/[14.5px] text-[#858D92]">{label}</h3>
       <h1 className={cn("font-medium text-xl/6", textStyle)}>{value}</h1>
     </div>
   );
--- src/components/Points/user-points/hero.tsx
+++ src/components/Points/user-points/hero.tsx
@@ -8,10 +8,7 @@ interface Props {
 const Hero = ({ address }: Props) => {
   return (
     <div className="flex flex-col gap-y-2 items-start w-full">
-      <h1 className="font-medium text-[2rem]/9">{shortenHash(address)}</h1>
-      {/* <p className="font-normal text-base/[22px] text-[#98B0C1]">
-        has been a user since 25 September 2024
-      </p> */}
+      <h1 className="font-medium text-[2rem]/9">{shortenHash(address, 5, 4)}</h1>
     </div>
   );
 };
